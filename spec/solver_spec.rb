12042451096873a2403df1cd31b007d9616fe36b
adeola003 <khadaf3@gmail.com>
2023-06-28 10:46:36 +0100

    add tests

---
+++ spec/solver_spec.rb
@@ -0,0 +1,53 @@
+require 'rspec'
+
+
+RSpec.describe Solver do
+  describe '#factorial' do
+    it 'returns the factorial of 0' do
+      expect(Solver.factorial(0)).to eq(1)
+    end
+
+    it 'returns the factorial of 1' do
+      expect(Solver.factorial(1)).to eq(1)
+    end
+
+    it 'returns the factorial of a positive number' do
+      expect(Solver.factorial(5)).to eq(120)
+      expect(Solver.factorial(10)).to eq(3628800)
+    end
+
+    it 'raises an error for negative numbers' do
+      expect { Solver.factorial(-5) }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#reverse' do
+    it 'reverses a word' do
+      expect(Solver.reverse('hello')).to eq('olleh')
+      expect(Solver.reverse('world')).to eq('dlrow')
+      expect(Solver.reverse('ruby')).to eq('ybur')
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" for numbers divisible by 3' do
+      expect(Solver.fizzbuzz(3)).to eq('fizz')
+      expect(Solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" for numbers divisible by 5' do
+      expect(Solver.fizzbuzz(5)).to eq('buzz')
+      expect(Solver.fizzbuzz(25)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" for numbers divisible by both 3 and 5' do
+      expect(Solver.fizzbuzz(15)).to eq('fizzbuzz')
+      expect(Solver.fizzbuzz(30)).to eq('fizzbuzz')
+    end
+
+    it 'returns the number as a string for other cases' do
+      expect(Solver.fizzbuzz(7)).to eq('7')
+      expect(Solver.fizzbuzz(11)).to eq('11')
+    end
+  end
+end
